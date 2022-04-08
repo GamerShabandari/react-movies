3a077075dc450351b655d74a584303655e48e45e
GamerSh <93838417+GamerShabandari@users.noreply.github.com>
2022-04-08 06:37:49 +0200

    ghpages

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import './App.css';
 import { Movies } from './components/Movies';
 import { ShowDetailedMovie } from './components/ShowDetailedMovie';
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { HashRouter, Link, Route, Routes } from 'react-router-dom';
 import "animate.css"
 
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <header>
           <Link to="/"><h1 className='animate__animated animate__fadeInDown'>Flixville</h1></Link>
         </header>
@@ -20,7 +20,7 @@ function App() {
           <Route path='/details/:id' element={<ShowDetailedMovie></ShowDetailedMovie>}></Route>
 
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
